2a9598be3a90301535178424d81bcb08b71c0bae
Natchu Peri <natchu.peri@aportio.com>
2025-04-16 11:42:27 +1200

    Add version file

---
+++ version.py
@@ -0,0 +1,12 @@
+# =========================================================
+# MIT license.
+#
+# (c) 2025 Aportio Developments Ltd.
+# =========================================================
+
+"""
+Current app version.
+"""
+
+# This version number should be incremented by bump2version
+VERSION = "v1.0.1"
