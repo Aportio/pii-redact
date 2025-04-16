69b47ed84560787d15f8af0730957f16405bc910
Natchu Peri <natchu.peri@aportio.com>
2025-04-16 18:42:42 +1200

    Fix test

--- redact_test.py
+++ redact_test.py
@@ -8,19 +8,22 @@
 Test redact text
 """
 
-import re
-
 from redact import PIIScrub
 
 
 def test_redact_urls():
     scrubber = PIIScrub()
-    url_pattern = scrubber.patterns["url"]
-    text = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"""
-    scrubbed_text = re.sub(url_pattern, "[REDACTED]", text)
-    scrubbed_text_from_tool = scrubber.scrub_text(text)
+    test_data = {  # text, expected_text
+        (
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd>",
+            "http://[[REDACTED]]/TR/xhtml1/DTD/[[REDACTED]]>",
+        ),
+        (
+            'xmlns="http://www.w3.org/1999/xhtml" ',
+            'xmlns="http://[[REDACTED]]/[REDACTED]/xhtml" ',
+        ),
+    }
+    for text, expected_text in test_data:
+        scrubbed_text_from_tool = scrubber.scrub_text(text)
 
-    print(scrubbed_text)
-
-    print(scrubbed_text_from_tool)
-    assert scrubbed_text == scrubbed_text_from_tool
+        assert scrubbed_text_from_tool == expected_text
