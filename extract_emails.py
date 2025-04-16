23cea221845a01c98128e7eaded5ebb6df7b7807
Natchu Peri <natchu.peri@aportio.com>
2025-04-16 12:44:44 +1200

    Add logic to wire up the components

--- extract_emails.py
+++ extract_emails.py
@@ -9,68 +9,15 @@ Tool to pre-process bulk import files into JSON
 """
 
 import json
-from typing import Optional
 import uuid
 
 from import_handlers import (
-    VALID_FIELDS,
     import_spreadsheet,
     import_pst,
 )
 
 
-def get_aruments() -> dict:
-    # create parser
-    parser = argparse.ArgumentParser(description=f"Bulk email import {VERSION}")
-
-    # add arguments to the parser
-    parser.add_argument("file", help="File to process")
-    parser.add_argument(
-        "-b", "--bucket", help="S3 bucket to store output", default="pst-emails-storage"
-    )
-    parser.add_argument(
-        "-p",
-        "--bucket_prefix",
-        help="S3 directory to store output. Defaults to customer id",
-    )
-    parser.add_argument(
-        "-m", "--map", help="Define mappings for input fields", action="append"
-    )
-
-    # parse the arguments
-    args = parser.parse_args()
-    return args
-
-
-def default_mapping() -> dict:
-    """
-    Return a default 1-1 mapping
-    """
-    mapping = {}
-    for key in VALID_FIELDS:
-        mapping[key] = [key]
-
-    return mapping
-
-
-def parse_field_mappings(raw_mappings: Optional[list]) -> dict:
-    """
-    Convert the list of mapping strings to a dict.
-    """
-    if not raw_mappings:
-        return default_mapping()
-
-    mapping = {}
-    for item in raw_mappings:
-        (key, value) = item.split("=")
-        mapping[key.lower()] = value.split(",")
-
-    return mapping
-
-
-def extract_emails(file_name: str, export_dir: str, raw_mappings: Optional[list]):
-
-    field_mappings = parse_field_mappings(raw_mappings)
+def extract_emails(file_name: str):
 
     # Decide whether to use PST or CSV/Excel to import the file
     payloads = []
@@ -89,7 +36,7 @@ def extract_emails(file_name: str, export_dir: str, raw_mappings: Optional[list]
 
         filename = str(uuid.uuid4())
         path = f"data/export/{filename}.json"
-        with open(path, 'w', encoding='utf-8') as f:
+        with open(path, "w", encoding="utf-8") as f:
             json.dump(payload, f, ensure_ascii=False, indent=4)
 
     print(f"\nTotal payloads: {item_count+1}")
--- main.py
+++ main.py
@@ -5,8 +5,22 @@
 # =========================================================
 
 import argparse
+import os
+import json
 
 from version import VERSION
+from pathlib import Path
+from redact_emails import redact_text
+from extract_emails import extract_emails
+
+EXPORT_DIR = "data/export/"
+HEADERS_WITH_PII = [
+    "from",
+    "sender",
+    "to",
+    "cc",
+    "subject",
+]
 
 
 def get_aruments() -> dict:
@@ -14,27 +28,75 @@ def get_aruments() -> dict:
     parser = argparse.ArgumentParser(description=f"PII Email Redactor {VERSION}")
 
     # add arguments to the parser
-    parser.add_argument("file", help="File to process")
+    parser.add_argument("file", help="File to process(PST/CSV)")
     parser.add_argument(
-        "-o", "--out", help="Output directory", default="redacted-emails"
+        "-o", "--outdir", help="Output directory", default="redacted-emails"
     )
-    parser.add_argument(
-        "-m", "--map", help="Define mappings for input fields", action="append"
-    )
-
     # parse the arguments
     args = parser.parse_args()
     return args
 
 
+def redact_payload(payload: dict) -> dict:
+    """
+    Redact all PII in the payload.
+
+    payload structure looks like this:
+    json_body = {
+        "headers": {
+            "from": sender,
+            "sender": sender,
+            "date": date,
+            "to": recipient,
+            "cc": cc,
+            "message_id": message_id.strip("\r\n "),
+            "subject": subject,
+            "content_type": content_type,
+        },
+        "envelope": {},
+        "plain": email_plain,
+        "html": email_html,
+        "attachments": [],
+    }
+    """
+    redacted_payload = payload
+    for key, value in payload.get("headers", {}):
+        if key in HEADERS_WITH_PII:
+            redacted_payload["headers"][key] = redact_text(
+                payload.get("headers", {}).get(key, "")
+            )
+    redacted_payload["plain"] = redact_text(payload.get("plain", ""))
+    redacted_payload["html"] = redact_text(payload.get("html", ""))
+    return redacted_payload
+
+
 def main():
     """ """
-    # Get the PST or CSV file from the directory
+    # Get the arguments.
+    args = get_aruments()
+
+    # Get the PST location
+    file_name = args.file
+
+    # Redacted email directory
+    redacted_dir = args.outdir
+
     # Extract the email content from the file
+    extract_emails(file_name=file_name)
+
     # Now loop through the files in the exported directory
     # Redact the email content from them
     # Save all the redacted data in a separate directory
-    print("")
+    pathlist = Path(EXPORT_DIR).rglob("*.json")
+    for path in pathlist:
+        with open(str(path), "r", encoding="utf-8") as f:
+            payload = json.load(f)
+            redacted_payload = payload
+            redacted_payload = redact_payload(payload)
+            filename = os.path.basename(f.name)
+            path = f"data/{redacted_dir}/{filename}"
+            with open(path, "w", encoding="utf-8") as outf:
+                json.dump(redacted_payload, outf, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
