353c15f38a588c4ed0ba451e155a475d3957388c
Natchu Peri <natchu.peri@aportio.com>
2025-04-16 18:05:46 +1200

    Refactor test

--- redact_test.py
+++ redact_test.py
@@ -15,9 +15,12 @@ from redact import PIIScrub
 
 def test_redact_urls():
     scrubber = PIIScrub()
-    url_pattern = PIIScrub.patterns["url"]
+    url_pattern = scrubber.patterns["url"]
     text = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"""
     scrubbed_text = re.sub(url_pattern, "[REDACTED]", text)
     scrubbed_text_from_tool = scrubber.scrub_text(text)
 
+    print(scrubbed_text)
+
+    print(scrubbed_text_from_tool)
     assert scrubbed_text == scrubbed_text_from_tool
