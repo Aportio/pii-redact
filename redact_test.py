9f1683603185bf6c82e389ffd495da1e1bf258df
James Mitchell <jtmitchell@users.noreply.github.com>
2025-05-30 18:08:28 +1200

    A2-4247 Improve PII detection and redaction (#5)

--- redact_test.py
+++ redact_test.py
@@ -13,17 +13,69 @@ from redact import PIIScrub
 
 def test_redact_urls():
     scrubber = PIIScrub()
-    test_data = {  # text, expected_text
+    # text, expected_text
+    test_data = [
         (
-            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd>",
-            "http://[[REDACTED]]/TR/xhtml1/DTD/[[REDACTED]]>",
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd",
+            "[REDACTED]",
         ),
         (
-            'xmlns="http://www.w3.org/1999/xhtml" ',
-            'xmlns="http://[[REDACTED]]/[REDACTED]/xhtml" ',
+            'xmlns="http://www.w3.org/1999/xhtml"',
+            'xmlns="[REDACTED]"',
         ),
-    }
-    for text, expected_text in test_data:
-        scrubbed_text_from_tool = scrubber.scrub_text(text)
+    ]
+    for test_num, (input_text, expected_text) in enumerate(test_data, start=1):
+        scrubbed_text_from_tool = scrubber.scrub_text(input_text)
 
-        assert scrubbed_text_from_tool == expected_text
+        assert scrubbed_text_from_tool == expected_text, f"#{test_num} failed"
+
+
+def test_redact_text():
+    scrubber = PIIScrub()
+    # text, expected_text
+    test_data = [
+        (
+            "John Smith, who was born 9th January, 2025, lives with David and Louise at the corner of East Street and 12th Ave.",
+            "[REDACTED], who was born [REDACTED], lives with [REDACTED] and [REDACTED] at the corner of [REDACTED] and [REDACTED].",
+        ),
+        (
+            "John Smith (9/01/2025), lives at 24 Walls St, London.",
+            "[REDACTED] ([REDACTED]), lives at [REDACTED], [REDACTED].",
+        ),
+        (
+            "John Smith (9/01/2025), lives at 24a Totara Avenue, Tauranga.",
+            "[REDACTED] ([REDACTED]), lives at [REDACTED], [REDACTED].",
+        ),
+        (
+            "John Smith (9/01/2025), lives at 24a Totara Avenue, Tauranga. Previously at 24 Walls St, London.",
+            "[REDACTED] ([REDACTED]), lives at [REDACTED], [REDACTED]. Previously at [REDACTED], [REDACTED].",
+        ),
+        (
+            "My bank account is 12-1234-1234567-12",
+            "My bank account is [REDACTED]",
+        ),
+        (
+            "My bank account is 12-1234-1234567-123",
+            "My bank account is [REDACTED]",
+        ),
+        (
+            "My IBAN account is GB82 WEST 1234 5698 7654 32",
+            "My [REDACTED] account is [REDACTED]",
+        ),
+        (
+            "My IBAN account is IE64 IRCE 9205 0112 3456 78 or IE64IRCE92050112345678",
+            "My [REDACTED] account is [REDACTED] or [REDACTED]",
+        ),
+        (
+            "My IBAN account is BI13 20001 10001 00001234567 89 or BI1320001100010000123456789",
+            "My [REDACTED] account is [REDACTED] or [REDACTED]",
+        ),
+        (
+            "My IBAN account is DE89-37040-04405-3201300",
+            "My [REDACTED] account is [REDACTED]",
+        ),
+    ]
+    for test_num, (input_text, expected_text) in enumerate(test_data, start=1):
+        scrubbed_text_from_tool = scrubber.scrub_text(input_text)
+
+        assert scrubbed_text_from_tool == expected_text, f"#{test_num} failed"
--- vendor/scrub.py
+++ vendor/scrub.py
@@ -20,20 +20,33 @@ except OSError:
 
 
 class Scrub:
+    REDACT_ENTIES = ["PERSON", "DATE", "LOC", "FAC", "ORG", "GPE"]
+
+    REDACTION_TEXT = "[REDACTED]"
+
     def __init__(self):
-        self.patterns = {
-            "email": r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b",
-            "phone": r"\b\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}\b",
-            "ssn": r"\b\d{3}[-]?\d{2}[-]?\d{4}\b",
-            "ip_address_v4": r"\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\b",
-            "ip_address_v6": r"\b(?:[0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}\b",
-            "hostname": r"\b(?:(?:[a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*((?:[A-Za-z]|(?:[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9]))\.(?:[a-zA-Z]{2,})|(?:xn--[A-Za-z0-9]+))\b",
-            "uuid": r"\b(?:[0-9a-fA-F]){8}-(?:[0-9a-fA-F]){4}-(?:[0-9a-fA-F]){4}-(?:[0-9a-fA-F]){4}-(?:[0-9a-fA-F]){12}\b",
-        }
+        self.patterns = [
+            ("email", r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"),
+            ("phone", r"\b\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}\b"),
+            ("ssn", r"\b\d{3}[-]?\d{2}[-]?\d{4}\b"),
+            (
+                "ip_address_v4",
+                r"\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\b",
+            ),
+            ("ip_address_v6", r"\b(?:[0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}\b"),
+            (
+                "hostname",
+                r"\b(?:(?:[a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*((?:[A-Za-z]|(?:[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9]))\.(?:[a-zA-Z]{2,})|(?:xn--[A-Za-z0-9]+))\b",
+            ),
+            (
+                "uuid",
+                r"\b(?:[0-9a-fA-F]){8}-(?:[0-9a-fA-F]){4}-(?:[0-9a-fA-F]){4}-(?:[0-9a-fA-F]){4}-(?:[0-9a-fA-F]){12}\b",
+            ),
+        ]
 
     def scrub_text(self, text: str) -> str:
         scrubbed_text = text
-        for category, pattern in self.patterns.items():
+        for category, pattern in self.patterns:
             if category == "phone":
                 matches = re.finditer(pattern, scrubbed_text)
                 for match in matches:
@@ -41,9 +54,9 @@ class Scrub:
 
                     # Remove parentheses from matched phone numbers
                     matched_phone = re.sub(r"^\((\d{3})\)$", r"\1", matched_phone)
-                    scrubbed_text = scrubbed_text.replace(matched_phone, "[REDACTED]")
+                    scrubbed_text = scrubbed_text.replace(matched_phone, self.REDACTION_TEXT)
             else:
-                scrubbed_text = re.sub(pattern, "[REDACTED]", scrubbed_text)
+                scrubbed_text = re.sub(pattern, self.REDACTION_TEXT, scrubbed_text)
 
         scrubbed_text = self.scrub_pii_with_nlp(scrubbed_text)
         return scrubbed_text
@@ -53,8 +66,8 @@ class Scrub:
         final_text = text
 
         for name in nlp_doc.ents:
-            if name.label_ == "PERSON":
-                final_text = re.sub(re.escape(name.text), "[REDACTED]", final_text)
+            if name.label_ in self.REDACT_ENTIES:
+                final_text = re.sub(re.escape(name.text), self.REDACTION_TEXT, final_text)
         return final_text
 
     def scrub(self, input_data: str | dict, original_format: str = "txt") -> str | dict:
